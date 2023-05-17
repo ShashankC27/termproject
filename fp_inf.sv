97a49e7fcc5e03cb15928206ec2196bf8fb469c4
ShashankC27 <125625115+ShashankC27@users.noreply.github.com>
2023-05-17 11:35:28 -0700

    Add files via upload

--- fp_inf.sv
+++ fp_inf.sv
@@ -5,7 +5,8 @@ interface fp_inf(input bit clk);
     logic [1:0] opcode;
     logic [31:0] c;
     logic start=1;
-	logic done_flag
+	logic done_flag;
+	
     modport slave (input a, b, opcode, start, output c , done_flag );
 
 parameter Q = 15;

7281c82a8627fbcf66a4c97243296aaef35d38d6
ShashankC27 <125625115+ShashankC27@users.noreply.github.com>
2023-05-23 12:09:27 -0700

    Add files via upload

--- fptop_tb.sv
+++ fptop_tb.sv
@@ -246,12 +246,12 @@ class scoreboard;
             endcase
             if(er != cr) begin
                 $display("Error : Output is not equal to input");
-                $display("Values are A = %f B=%f and opcode=%b",ar,br,opcode);
+                $display("Values are A = %f B=%f and opcode=%b",ar,br,trans.opcode);
                 $display("Actual Value = %f and Expected Value = %f",cr,er);
             end
             else begin
                 $display("Output is equal to input");
-                $display("Values are A = %f B=%f and opcode=%b",ar,br,opcode);
+                $display("Values are A = %f B=%f and opcode=%b",ar,br,trans.opcode);
                 $display("Actual Value = %f and Expected Value = %f",cr,er);
             end
         end
